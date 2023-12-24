c47dd09f57fc7e833782f21d0779e85651450a70
Timofei Balduev <balduev.desco@mail.ru>
2023-12-24 04:29:33 +0300

    minor changes

--- src/__main__.py
+++ src/__main__.py
@@ -6,23 +6,21 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from src.routers import router
 import logging
-from src.db.queries.orm import create_tables
 
 
 async def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
-    load_dotenv('.env')
+    load_dotenv(".env")
     dp = Dispatcher(storage=MemoryStorage())
-    bot = Bot(token=os.getenv('TOKEN'))
+    bot = Bot(token=os.getenv("TOKEN"))
 
     dp.include_routers(router)
-    await create_tables()
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         asyncio.run(main())
 
-    except(SystemExit, KeyboardInterrupt):
-        print('Bot stopped')
+    except (SystemExit, KeyboardInterrupt):
+        print("Bot stopped")
